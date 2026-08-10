91bf86c4e5ae957a04af7c99aa30a6b145ce7107
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-08-10 21:49:41 +0100

    Calculate and display total cost of order items

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,18 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY     ITEM                TOTAL");
+
+let totalCost = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const itemTotal = quantity * unitPricePence;
+  totalCost += itemTotal;
+
+  console.log(
+    `${quantity}       ${itemName.padEnd(20)} ${(itemTotal / 100).toFixed(2)}`
+  );
+});
+
+console.log(`\nTotal: ${(totalCost / 100).toFixed(2)}`);
