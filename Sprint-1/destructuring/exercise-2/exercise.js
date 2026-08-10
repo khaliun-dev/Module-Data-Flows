5d0d0eb1974a479eddd44d832a9f4463ab9040d1
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-08-10 21:31:22 +0100

    Add tasks to filter Hogwarts students by house and occupation

--- Sprint-1/destructuring/exercise-2/exercise.js
+++ Sprint-1/destructuring/exercise-2/exercise.js
@@ -70,3 +70,22 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+//Task 1
+
+hogwarts.forEach(({ firstName, lastName, house }) => {
+  if (house === "Gryffindor") {
+    console.log(`${firstName} ${lastName}`);
+  }
+});
+
+//Task 2
+
+hogwarts.forEach(({ firstName, lastName, occupation, pet }) => {
+  if (occupation === "Teacher" && pet) {
+    console.log(`${firstName} ${lastName}`);
+  }
+});
+
+
+
